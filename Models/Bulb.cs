4ca2901e3b67d4f0cdf4a88533bfec613202bac7
Bhanu Kiran <bhanukiran.g@keka.com>
2025-12-14 13:44:29 +0530

    Added bulb.cs

---
+++ Models/Bulb.cs
@@ -0,0 +1,7 @@
+namespace SwitchBoardConsoleApp.Models
+{
+    public class Bulb : ApplianceBase
+    {
+        public Bulb(int id) : base(id, "Bulb") { }
+    }
+}
--- obj/Debug/net8.0/SwitchBoardConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SwitchBoardConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchBoardConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c5b12e7a1752333cb217c86cf46c421b679625c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa1f2e2c58586e70895bb47fc1d0796666c2941")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchBoardConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchBoardConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
